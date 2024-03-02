7b177e4299b5d6ba74eb1448d3eb6b49086238e2
jaydenpiao <jaydenpiao@gmail.com>
2024-03-02 12:34:51 -0800

    homepage and navbar

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,8 +1,12 @@
+import Home from "./components/Home";
 import Input from "./components/Input";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
+      <Navbar />
+      <Home />
       <Input />
     </div>
   );
