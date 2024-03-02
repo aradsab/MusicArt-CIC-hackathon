3058d13f541f275406cb4b27c85aa5a14e705390
Ayush Patel <104913698+Ayush200423@users.noreply.github.com>
2024-03-02 14:26:24 -0800

    decode react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Home from "./components/Home";
+import Input from "./components/Input";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <Input />
+    </div>
+  );
+}
+
+export default App;
